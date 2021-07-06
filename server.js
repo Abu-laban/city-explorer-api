b92cd9dc23ec6d93056bc4755cc6bc61db0c86d5
Tariq Abulaban <tariqabulaban@hotmail.com>
2021-07-06 16:45:43 +0300

    add moviesAPI

--- server.js
+++ server.js
@@ -48,6 +48,30 @@ function weatherHandler(request, response) {
 
 };
 
+server.get('/movies', moviesHandler);
+
+function moviesHandler(request, response) {
+
+    const sQuery = request.query.cityName
+
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_KEY}&query=${sQuery}`
+
+    axios
+        .get(url)
+        .then(moviesData => {
+
+            response.status(200).send(moviesData.data.results.map(obj => {
+                return new Movies(obj)
+            }))
+
+        })
+        .catch(error => {
+            response.status(500).send(error)
+        })
+
+
+};
+
 server.get('*', (request, response) => {
     response.status(500).send('NOT FOUND')
 })
@@ -58,4 +82,18 @@ class Weather {
         this.date = day.datetime;
     }
 }
+
+class Movies {
+    constructor(obj) {
+        this.title = obj.original_title;
+        this.overview = obj.overview;
+        this.average_votes = obj.vote_average;
+        this.total_votes = obj.vote_count;
+        this.image_url = `https://image.tmdb.org/t/p/w500/${obj.poster_path}`;
+        this.popularity = obj.popularity;
+        this.released_on = obj.release_date;
+    }
+
+}
+
 server.listen(PORT, () => console.log(`listening on ${PORT}`));
