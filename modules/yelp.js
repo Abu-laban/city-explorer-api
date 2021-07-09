dc510a48b1249ec82c73adc32cb1102df1e25efc
Tariq Abulaban <tariqabulaban@hotmail.com>
2021-07-09 14:18:50 +0300

    edit lab10 yelpAPI

--- modules/yelp.js
+++ modules/yelp.js
@@ -7,32 +7,29 @@ function yelpHandler(request, response) {
 
     const sQuery = request.query.cityName
 
-    let url = `https://api.yelp.com/v3/businesses/search`;
-
     let key = process.env.YELP_KEY;
 
-    if (cache[`y ${sQuery}`] !== undefined) {
-        response.status(200).send(cache[`y ${sQuery}`]);
+    if (cache[`yelp ${sQuery}`] !== undefined) {
+        response.status(200).send(cache[`yelp ${sQuery}`]);
     } else {
-        axios
-            .get(url, {
-                headers: {
-                    Authorization: `Bearer ${key}`
-                },
-                params: {
-                    location: { sQuery },
-
-                }
-            })
-            .then(yelpData => {
-
-                cache[`y ${sQuery}`] = yelpData.data.businesses.map(yelpObj => new Yelp(yelpObj))
-
-                response.status(200).send(yelpData.data.businesses.map(yelpObj => {
-                    return new Yelp(yelpObj);
-                }))
-
-            })
+        let yelpREST = axios.create({
+            baseURL: "https://api.yelp.com/v3/",
+            headers: {
+                Authorization: `Bearer ${key}`,
+                "Content-type": "application/json",
+            },
+        })
+        yelpREST("/businesses/search", {
+            params: {
+                location: sQuery,
+            },
+        }).then(({ data }) => {
+            let { businesses } = data
+            cache[`yelp ${sQuery}`] = businesses.map(yelpObj => new Yelp(yelpObj))
+            response.status(200).send(businesses.map(yelpObj => {
+                return new Yelp(yelpObj);
+            }))
+        })
             .catch(error => {
                 response.status(500).send(error)
             })
